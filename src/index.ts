85aaa75565c160638e41f457250db634c73c214e
Zacknetic <zackavino@gmail.com>
2021-10-03 13:38:30 -0400

    added more exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 export { ControllerGenerator } from './ControllerGenerator';
 export { BaseController } from './DeviceControllers/BaseController'
-export { ICustomProtoDevice, CustomCompleteDeviceProps, DirectCommand, ICommandOptions, IDeviceAPI, IProtoDevice, IDeviceCommand } from './types'
+export { 
+    ICustomProtoDevice, 
+    CustomCompleteDeviceProps, 
+    DirectCommand, 
+    ICommandOptions, 
+    IDeviceAPI, 
+    IProtoDevice, 
+    IDeviceState,
+    DeviceWriteStatus,
+    IDeviceCommand } from './types'
